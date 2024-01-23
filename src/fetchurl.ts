f546bafe9e741a98eae763e113fed07e97d30851
nyarla <nyarla@kalaclista.com>
2024-01-23 16:10:42 +0900

    feat(fetchurl): add isSameHref function

--- src/fetchurl.ts
+++ src/fetchurl.ts
@@ -2,6 +2,13 @@ function shouldUpdate(lastUpdated, now: number): bool {
   return now >= lastUpdated + Expiration;
 }
 
+function isSameHref(a, b) {
+  const aURL = a.replace(/^https?:/, '');
+  const bURL = b.replace(/^https?:/, '');
+
+  return aURL === bURL;
+}
+
 function testShouldUpdate() {
   if (shouldUpdate(0, 0)) {
     throw new Error("in this case, this result should be false");
@@ -13,5 +20,15 @@ function testShouldUpdate() {
 
   if (!shouldUpdate(0, Expiration + 1)) {
     throw new Error("in this case, this result should be true");
+  }
+}
+
+function testIsSameHref() {
+  if (!isSameHref('http://example.com', 'https://example.com')) {
+    throw new Error("in this case, this result should be true");
+  }
+
+  if (isSameHref('http://example.com/foo', 'https://example.com/bar')) {
+    throw new Error("in this case, this result should be false");
   }
 }
