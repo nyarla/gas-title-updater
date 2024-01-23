a73bf050965eb916b88f1e5b82f4216fd7c0241f
nyarla <nyarla@kalaclista.com>
2024-01-23 16:25:52 +0900

    feat(fetchurl): add fetchUrl function

--- src/fetchurl.ts
+++ src/fetchurl.ts
@@ -9,6 +9,47 @@ function isSameHref(a, b) {
   return aURL === bURL;
 }
 
+function fetchUrl(href, lastUpdated): [ number, string, string ] {
+  const params = {
+    method: 'get',
+    headers: {
+      'If-Modified-Since': new Date(lastUpdated).toUTCString(),
+    },
+    muteHTTPExceptions: true,
+    followRedirect: false,
+  };
+
+  let statusCode = 0;
+  let location = href;
+  let content = "";
+
+  try {
+    for (let retry = 0; retry < 5; retry++) {
+      const response = UrlFetchApp.fetch(location, params);
+      
+      statusCode = response.getResponseCode();
+     
+      if (301 <= statusCode && statusCode <= 399) {
+        location = response.getHeaders().Location;
+        continue;
+      }
+
+      if (statusCode === 304) {
+        break;
+      }
+
+      content = response.getContentText();
+      break;
+    }
+  }
+  catch (err) {
+    statusCode = 599;
+    content = err.toString();
+  }
+
+  return [statusCode, location, content];
+}
+
 function testShouldUpdate() {
   if (shouldUpdate(0, 0)) {
     throw new Error("in this case, this result should be false");
@@ -31,4 +72,10 @@ function testIsSameHref() {
   if (isSameHref('http://example.com/foo', 'https://example.com/bar')) {
     throw new Error("in this case, this result should be false");
   }
+}
+
+function testFetchUrl() {
+  const [ status, href, content ] = fetchUrl('https://example.com', 0);
+
+  Logger.log(JSON.stringify({ status, href, content }));
 }
